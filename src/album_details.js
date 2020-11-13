9b09184cfaa583ecd5a7d091cde3b9d77a7e3fca
Erwann <erwann44110@gmail.com>
2020-11-13 12:29:24 +0100

    fix icon

--- src/album_details.js
+++ src/album_details.js
@@ -24,7 +24,7 @@ class AlbumDetails extends React.Component {
                                     <td>Titre 1</td>
                                     <td className="is-icon">
                                         <a href="#">
-                                            <i className="fa fa-stars"/>
+                                            <i className="fas fa-stars"/>
                                         </a>
                                     </td>
                                 </tr>
@@ -33,7 +33,7 @@ class AlbumDetails extends React.Component {
                                     <td>Titre 2</td>
                                     <td className="is-icon">
                                         <a href="#">
-                                            <i className="fa fa-stars"/>
+                                            <i className="fas fa-stars"/>
                                         </a>
                                     </td>
                                 </tr>
